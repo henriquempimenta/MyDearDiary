0f3f5b044ad060790c7a0c19cea78e256ccb214a
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2025-08-08 19:00:24 -0300

    Fix: Provide DiaryRepository to the app and rebuild APK

--- lib/app/my_dear_diary_app.dart
+++ lib/app/my_dear_diary_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../data/repositories/diary_repository.dart';
 import 'app_router.dart';
 
 class MyDearDiaryApp extends StatelessWidget {
@@ -6,11 +8,14 @@ class MyDearDiaryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      title: 'My Dear Diary',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (context) => DiaryRepository(),
+      child: MaterialApp.router(
+        routerConfig: router,
+        title: 'My Dear Diary',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
       ),
     );
   }
